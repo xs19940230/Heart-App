e7ff474761fb7b34dc7c07a4dc5035e24e864814
xs <xingshuo1994@outlook.com>
2025-09-21 15:12:57 +0800

    git test

---
+++ Heart Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Heart Watch App
+//
+//  Created by xs on 21.09.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world! git test!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Heart Watch App/HeartApp.swift
@@ -0,0 +1,17 @@
+//
+//  HeartApp.swift
+//  Heart Watch App
+//
+//  Created by xs on 21.09.25.
+//
+
+import SwiftUI
+
+@main
+struct Heart_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
